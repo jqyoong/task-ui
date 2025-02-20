1094c65680f0ff43ecf7109adb8b40b95ef75cae
Yoong Jun Quan <yoongjunquan@gmail.com>
2025-02-20 16:12:54 +0800

    feat(web): include tooltip and providers components

---
+++ app/components/providers.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
+import { TooltipProvider } from '@/components/ui/tooltip';
+
+export function Providers({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      <TooltipProvider>{children}</TooltipProvider>
+    </NextThemesProvider>
+  );
+}
